bf121bb57197f512e55ccc3ff703b4baacf2ff48
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-06 15:00:40 -0700

    Done.

---
+++ db/migrate/20200906215745_add_passenger_id_to_passengers.rb
@@ -0,0 +1,5 @@
+class AddPassengerIdToPassengers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :passengers, :passenger_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200906215527) do
+ActiveRecord::Schema.define(version: 20200906215953) do
 
   create_table "passengers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "taxi_id"
     t.integer  "ride_id"
+    t.integer  "passenger_id"
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
